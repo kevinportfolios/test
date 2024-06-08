3b36c04b7c2e92b60cfbaae2e431a1b39fca85bb
kevinportfolios <116961969+kevinportfolios@users.noreply.github.com>
2024-06-08 11:27:29 +0000

    update

--- tests/v5/log.php
+++ tests/v5/log.php
@@ -131,3 +131,34 @@ function testlogger($log){
 
 
 }
+
+
+function loggertest($log){
+	$time=time()+(7*3600);
+	$folderName = 'test';  // folder name
+	// $filename= $folderName . '/' .date('Y-m-d',$time).'log.txt';
+	$filename= $folderName . '/' .'testlog3.txt';
+	if(!file_exists($filename)){
+	   file_put_contents($filename,'');
+	}
+	
+	$contents = file_get_contents($filename);
+	$contents .= "$log\r";
+
+    file_put_contents($filename,$contents);
+}
+
+function orderloggertest($log){
+	$time=time()+(7*3600);
+	$folderName = 'test';  // folder name
+	// $filename= $folderName . '/' .date('Y-m-d',$time).'orderlog.txt';
+	$filename= $folderName . '/' .'testorderlog3.txt';
+	if(!file_exists($filename)){
+	   file_put_contents($filename,'');
+	}
+	
+	$contents = file_get_contents($filename);
+	$contents .= "$log\r\r";
+
+    file_put_contents($filename,$contents);
+}
