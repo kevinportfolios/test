4ea4783fca844bc9da66fa6e3cabb4cf70a6ec74
kevinportfolios <116961969+kevinportfolios@users.noreply.github.com>
2024-12-08 14:18:40 +0000

    update

--- tests/v5/key_secret.php
+++ tests/v5/key_secret.php
@@ -1,5 +1,5 @@
 <?php
-$key='5oJwyvT7lKLry2u2Sr';
-$secret='M8M1EGNUjIFxeC4pZ7gfKwA7B1g1k4b58yGc';
+$key='eTPmTZK2nJhMAh8Y0Q';
+$secret='kNNA8RTY5NDrkTvfDVhnT38RHzhJiulafhIG';
 
 // php sma50-2.php '5oJwyvT7lKLry2u2Sr' 'M8M1EGNUjIFxeC4pZ7gfKwA7B1g1k4b58yGc'
